70b7a0dcbad02f268f52a72d9995431f6200ea3c
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2024-05-22 18:52:02 +0200

    Names first pass

--- src/main.py
+++ src/main.py
@@ -36,10 +36,22 @@ ICON_MINUS = '\uE001'
 map_img = pygame.image.load("./mapa_gmina_fredropol.png").convert()
 map_original_dim = map_img.get_size()
 map_offset = (0, 0)
-map_scale = 1
+map_scale = 0.25
 scaled_map = scale_map(map_img, map_original_dim, map_scale)
 scaled_map_dim = scaled_map.get_size()
 MAP_SCALE_FACTOR = 1.25
+
+def scale_map_up():
+    global map_scale, scaled_map, scaled_map_dim, MAP_SCALE_FACTOR
+    map_scale *= MAP_SCALE_FACTOR
+    scaled_map = scale_map(map_img, map_original_dim, map_scale)
+    scaled_map_dim = scaled_map.get_size()
+
+def scale_map_down():
+    global map_scale, scaled_map, scaled_map_dim, MAP_SCALE_FACTOR
+    map_scale /= MAP_SCALE_FACTOR
+    scaled_map = scale_map(map_img, map_original_dim, map_scale)
+    scaled_map_dim = scaled_map.get_size()
 
 target_fps = 60
 target_frame_time = 1/target_fps 
@@ -67,19 +79,29 @@ def parse_lat_long(lat_long):
     split = lat_long.split(' ')
     return degrees_from_dms(split[0]), degrees_from_dms(split[1])
 
+# latitude(lat)=y and longitude(long)=x
 places_excel['lat'], places_excel['long'] = zip(*places_excel['współrzędne geograficzne'].apply(func=parse_lat_long))
-min_lat,  max_lat  = min(places_excel['lat']),  max(places_excel['lat'])
-min_long, max_long = min(places_excel['long']), max(places_excel['long'])
 
-def render_name(lat, long, name):
-    # TODO(Pawel Hermansdorfer): Finish
+#                    min     max
+min_long, max_long = 22.568, 22.82  # x
+min_lat, max_lat   = 49.568, 49.732 # y
+
+def render_name(map_pos, map_dim, lat, long, name):
+    # TODO(Pawel Hermansdorfer): Cleanup
+    global font, screen, min_lat, max_lat, min_long, max_long
+
     def map(a, start1, stop1, start2, end2):
         return start2 + (((end2 - start2) / (stop1 -start1 )) * (a - start1))
 
-    map_x = map(lat, min_lat, max_lat, 0, max_size[0])
-    map_y = map(long, min_long, max_long, 0, max_size[1])
+    map_x = map(long, min_long, max_long, map_pos[0], map_pos[0] + map_dim[0])
 
+    # y must be inverse. y in screen space is growing downward, and latitude upward.
+    map_y = map_pos[1] + (map_dim[1] - map(lat,  min_lat,  max_lat,  0, map_dim[1]))
 
+    color = (0, 0, 0)
+    rendered_name = font.render(name, True, color)
+    pygame.draw.circle(screen, color, (map_x, map_y), 5)
+    screen.blit(rendered_name, (map_x, map_y))
 
 while is_running:
     ########################################
@@ -106,10 +128,22 @@ while is_running:
                               map_offset[1] + 1/map_scale * (mouse_pos[1] - mouse_drag_start[1]))
 
         elif event.type == pygame.KEYDOWN:
+            # alt+enter = fullscreen
             if event.key == pygame.K_RETURN and alt_is_pressed:
                 toggle_fullscreen(screen)
+
+            # alt + q = quit
             if event.key == pygame.K_q and alt_is_pressed:
                 is_running = False
+
+        # scroll
+        elif event.type == pygame.MOUSEWHEEL:
+            # event.x - horizontal
+            # event.y - vertical
+            if event.y > 0:
+                scale_map_up()
+            elif event.y < 0:
+                scale_map_down()
 
     ########################################
     # Update
@@ -127,23 +161,21 @@ while is_running:
     # Render
     screen.fill((50, 40, 40))
 
-    blit_pos = (screen_dim[0]/2 - scaled_map_dim[0]/2 + offset[0]*map_scale,
+    map_blit_pos = (screen_dim[0]/2 - scaled_map_dim[0]/2 + offset[0]*map_scale,
                 screen_dim[1]/2 - scaled_map_dim[1]/2 + offset[1]*map_scale)
-    screen.blit(scaled_map,  blit_pos)
+    screen.blit(scaled_map,  map_blit_pos)
 
     fps_text = font.render(f'FPS: {1/frame_time if frame_time != 0 else 0:.2f}', True, (0, 255, 0))
     screen.blit(fps_text, (0,0))
 
+    places_excel[['lat', 'long', 'nazwa główna']].apply(axis=1, func=lambda x: render_name(map_blit_pos, scaled_map_dim, *x))
+
     # UI
     if button(ui, ICON_MINUS + "###SCALE_DOWN", pygame.Rect(screen_dim[0] - 35, screen_dim[1] - 35, 30, 30), font_icons):
-        map_scale /= MAP_SCALE_FACTOR
-        scaled_map = scale_map(map_img, map_original_dim, map_scale)
-        scaled_map_dim = scaled_map.get_size()
+        scale_map_down()
 
     if button(ui, ICON_PLUS + "###SCALE_UP", pygame.Rect(screen_dim[0] - 75, screen_dim[1] - 35, 30, 30), font_icons):
-        map_scale *= MAP_SCALE_FACTOR
-        scaled_map = scale_map(map_img, map_original_dim, map_scale)
-        scaled_map_dim = scaled_map.get_size()
+        scale_map_up()
 
     if button(ui, 'RESET###RESET', pygame.Rect(screen_dim[0] - 185, screen_dim[1] - 35, 100, 30)):
         map_offset = (0, 0)
