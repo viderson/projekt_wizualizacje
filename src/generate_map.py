c82b4dd81a28808250266fcfd4c35053c2253b97
EriK013 <estepien1307@gmail.com>
2024-06-30 16:52:09 +0200

    Geojason moze

--- src/generate_map.py
+++ src/generate_map.py
@@ -7,71 +7,40 @@ from dataHandler import readExcelData, getCoordinates, getBounds, getPointDescri
 
 filepath = 'dane_gmina_fredropol.xlsx'
 readExcelData(filepath)
-
 coordinates_list = getCoordinates()
 
 map_fedropol = folium.Map(location=coordinates_list[0], zoom_start=12)
 map_fedropol.fit_bounds(getBounds(coordinates_list))
-markers = folium.FeatureGroup(name='Markers').add_to(map_fedropol)
+
+features = []
 for i, coord in enumerate(coordinates_list):
-    color = None
-    icon = None
-    popup_text = getPointDescription(['identyfikator PRNG', 'nazwa główna', 'rodzaj obiektu' ], i)
-    if 'las' in popup_text:
-        color = 'green'
-        icon = 'tree'
+    popup_text = getPointDescription(['identyfikator PRNG', 'nazwa główna', 'rodzaj obiektu'], i)
 
-    elif 'potok' in popup_text:
-        color = 'blue'
-        icon = 'water'
+    # Create GeoJSON feature
+    feature = {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": coord[::-1]
+        },
+        "properties": {
+            "name" : popup_text,
+        }
+    }
+    features.append(feature)
 
-    elif 'góra' in popup_text or 'skała' in popup_text or 'masyw' in popup_text:
-        color = 'gray'
-        icon = 'mountain'
+# Create a GeoJSON Feature Collection
+feature_collection = {
+    "type": "FeatureCollection",
+    "features": features,
+}
 
-    elif 'pole' in popup_text or 'dolina'  in popup_text or 'jar' in popup_text:
-        color = 'orange'
-        icon = 'seedling'
-
-    elif 'kopiec' in popup_text:
-        color = 'red'
-        icon = 'landmark'
-
-    elif 'łąki' in popup_text:
-        color = 'darkgreen'
-        icon = 'leaf'
-
-    elif 'przełęcz' in popup_text:
-        color = 'purple'
-        icon = 'exchange-alt'
-
-    elif 'region naturalny' in popup_text:
-        color = 'darkpurple'
-        icon = 'global'
-
-    elif 'uroczysko-dawna miejscowość' in popup_text:
-        color = 'beige'
-        icon = 'home'
-
-    marker = folium.Marker(
-        coord,
-        name=popup_text,
-        popup=popup_text,
-        icon=folium.Icon(color=color, icon=icon, prefix='fa')
-    ).add_to(markers)
-
-
-    folium.Circle(
-        location=coord,
-        radius=100,  # Promień w metrach
-        color=color,
-        fill=True,
-        fill_color=color
-    ).add_to(map_fedropol)
+gjson = folium.GeoJson(feature_collection).add_to(map_fedropol) # Stwórz GeoJson
+folium.features.GeoJsonPopup(fields=["name"], labels=False).add_to(gjson) # Dodaj popupy
 
 # Dodaj funkcjonalność wyszukiwania
 search = Search(
-    layer=markers,
+    layer=gjson,
     geom_type='Point',
     placeholder="Wyszukaj...",
     collapsed=False,
