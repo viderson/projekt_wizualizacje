8764fdb38a86593221a677078a7aa70d7a9679a1
viderson <163771318+viderson@users.noreply.github.com>
2024-07-04 21:11:53 +0200

    Update generate_map.py

--- src/generate_map.py
+++ src/generate_map.py
@@ -13,17 +13,18 @@ coordinates_list = getCoordinates()
 map_fedropol = folium.Map(location=coordinates_list[0], zoom_start=12)
 map_fedropol.fit_bounds(getBounds(coordinates_list))
 
-main_group = folium.FeatureGroup(name='All Locations').add_to(map_fedropol)
+main_group = folium.FeatureGroup(name='All Locations', show=False).add_to(map_fedropol)
+
 groups = {
-    'forest': folium.plugins.FeatureGroupSubGroup(main_group, 'las').add_to(map_fedropol),
-    'stream': folium.plugins.FeatureGroupSubGroup(main_group, 'potok').add_to(map_fedropol),
-    'mountain': folium.plugins.FeatureGroupSubGroup(main_group, 'góra').add_to(map_fedropol),
-    'mound': folium.plugins.FeatureGroupSubGroup(main_group, 'kopiec').add_to(map_fedropol),
-    'field': folium.plugins.FeatureGroupSubGroup(main_group, 'pole').add_to(map_fedropol),
-    'meadow': folium.plugins.FeatureGroupSubGroup(main_group, 'łąki').add_to(map_fedropol),
-    'pass': folium.plugins.FeatureGroupSubGroup(main_group, 'przełęcz').add_to(map_fedropol),
-    'natural_region': folium.plugins.FeatureGroupSubGroup(main_group, 'region naturalny').add_to(map_fedropol),
-    'historical_settlement': folium.plugins.FeatureGroupSubGroup(main_group, 'uroczysko-dawna miejscowość').add_to(map_fedropol),
+    'forest': folium.plugins.FeatureGroupSubGroup(main_group, 'las', show=True).add_to(map_fedropol),
+    'stream': folium.plugins.FeatureGroupSubGroup(main_group, 'potok', show=True).add_to(map_fedropol),
+    'mountain': folium.plugins.FeatureGroupSubGroup(main_group, 'góra', show=True).add_to(map_fedropol),
+    'mound': folium.plugins.FeatureGroupSubGroup(main_group, 'kopiec', show=True).add_to(map_fedropol),
+    'field': folium.plugins.FeatureGroupSubGroup(main_group, 'pole', show=True).add_to(map_fedropol),
+    'meadow': folium.plugins.FeatureGroupSubGroup(main_group, 'łąki', show=True).add_to(map_fedropol),
+    'pass': folium.plugins.FeatureGroupSubGroup(main_group, 'przełęcz', show=True).add_to(map_fedropol),
+    'natural_region': folium.plugins.FeatureGroupSubGroup(main_group, 'region naturalny', show=True).add_to(map_fedropol),
+    'historical_settlement': folium.plugins.FeatureGroupSubGroup(main_group, 'uroczysko-dawna miejscowość', show=True).add_to(map_fedropol),
 }
 
 default_icon = "https://api.geoapify.com/v1/icon/?type=material&color=%230c6f08&icon=help&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
@@ -39,48 +40,40 @@ for i, coord in enumerate(coordinates_list):
         color = 'green'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%230c6f08&icon=tree&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['forest']
-
     elif 'potok' in popup_text:
         color = 'blue'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%230ee5e9&icon=water&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['stream']
-
     elif 'góra' in popup_text or 'skała' in popup_text or 'masyw' in popup_text:
         color = 'gray'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%2361696a&icon=mountain&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['mountain']
-
     elif 'kopiec' in popup_text:
         color = 'red'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%238cb57f&icon=terrain&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['mound']
-
     elif 'pole' in popup_text or 'dolina' in popup_text or 'jar' in popup_text or 'pola' in popup_text:
         color = 'orange'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%239be351&icon=seedling&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['field']
-
     elif 'łąki' in popup_text:
         color = 'darkgreen'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%23d3f035&icon=grass&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['meadow']
-
     elif 'przełęcz' in popup_text:
         color = 'purple'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%23b935f0&icon=route&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['pass']
-
     elif 'region naturalny' in popup_text:
         color = 'darkpurple'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%23fdfdfd&icon=cruelty_free&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['natural_region']
-
     elif 'uroczysko-dawna miejscowość' in popup_text:
         color = 'beige'
         icon = "https://api.geoapify.com/v1/icon/?type=material&color=%238f3d3d&icon=home&iconType=awesome&iconSize=large&scaleFactor=2&apiKey=769fdc58c4cc45b3a87a679303341049"
         group = groups['historical_settlement']
 
-    # Create GeoJSON feature
+    
     feature = {
         "type": "Feature",
         "geometry": {
@@ -88,10 +81,10 @@ for i, coord in enumerate(coordinates_list):
             "coordinates": coord[::-1]
         },
         "properties": {
-            "time" : getTimeStamps(i),
-            "name" : popup_text,
+            "time": getTimeStamps(i),
+            "name": popup_text,
             "popup": popup_text,
-            "markerColor" : color,
+            "markerColor": color,
             "icon": "marker",
             "iconstyle": {
                 "iconUrl": f'{icon}',
@@ -100,12 +93,24 @@ for i, coord in enumerate(coordinates_list):
         }
     }
     features.append(feature)
-    folium.Marker(location=coord, popup=popup_text, icon=folium.CustomIcon(icon, icon_size=(31, 46))).add_to(group)
+
+    
+    html_popup = folium.Html(popup_text, script=True)
+    popup = folium.Popup(html_popup, max_width=300)
+
+    
+    folium.Marker(
+        location=coord,
+        popup=popup,
+        icon=folium.CustomIcon(icon, icon_size=(31, 46))
+    ).add_to(group)
+
 
 feature_collection = {
     'type': 'FeatureCollection',
     'features': features
 }
+
 
 gjson = folium.GeoJson(
     feature_collection,
@@ -113,6 +118,7 @@ gjson = folium.GeoJson(
     popup=folium.GeoJsonPopup(fields=['name'], labels=False),
     name="geojson",
 ).add_to(map_fedropol)
+
 
 search = Search(
     layer=gjson,
@@ -124,6 +130,7 @@ search = Search(
     show=False
 ).add_to(map_fedropol)
 
+
 TimestampedGeoJson(
     feature_collection,
     period='P3M',
@@ -132,7 +139,8 @@ TimestampedGeoJson(
     loop=False
 ).add_to(map_fedropol)
 
-folium.LayerControl(collapsed=False).add_to(map_fedropol)
+
+folium.LayerControl(collapsed=True).add_to(map_fedropol)
 
 map_fedropol.save("fedropol_map.html")
-#webbrowser.open("fedropol_map.html")
+webbrowser.open("fedropol_map.html")
